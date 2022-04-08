25bb58d8d3650ae8e35fe6a4a8cf5cca49f3b415
lm359x <lm359x@gmail.com>
2022-04-08 03:48:58 +0300

    added commands not to forget at lesson

--- src/main/java/test/PrintSomething.java
+++ src/main/java/test/PrintSomething.java
@@ -1,6 +1,9 @@
 package test;
 
 public class PrintSomething {
+    //gradle build
+    //docker images
+    //docker run -it mycoolapp
     public static void main(String[] args) {
         System.out.println("am i working?");
     }
